75e076c5ee327fcb7fe4914ccc5cdda164388526
Proxtx <39201896+Proxtx@users.noreply.github.com>
2023-04-20 19:08:39 +0200

    improve cookie default

--- static/lib/guiLoader.js
+++ static/lib/guiLoader.js
@@ -2,6 +2,9 @@ window.guiLoaded = [];
 
 import { loadPack } from "/modules/uibuilder/main.js";
 
+cookie.default =
+  "; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/; SameSite=None; Secure";
+
 await loadPack("/modules/material/components/pack.json", {
   urlPrefix: "/modules/material/",
   customStyleSheets: ["../../lib/main.css"],
